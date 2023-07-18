a02beb24301b9b9cd2fcf80087e13ebcaea2c738
PavanKalyan <pavankalyan6md@gmail.com>
2023-07-18 13:27:22 +0530

    Commited

--- studentInfoProject/src/main/java/com/springboot/studentInfoProject/Configuration/StudentConfig.java
+++ studentInfoProject/src/main/java/com/springboot/studentInfoProject/Configuration/StudentConfig.java
@@ -1,35 +1,62 @@
 package com.springboot.studentInfoProject.Configuration;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
-public class StudentConfig extends WebSecurityConfigurerAdapter {
+public class StudentConfig {
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
+
+
+    @Bean
+    public UserDetailsService userDetailsService(PasswordEncoder encoder) {
+        InMemoryUserDetailsManager userDetailsManager = new InMemoryUserDetailsManager();
+
+        // Admin User
+        userDetailsManager.createUser(User.builder()
+                .username("admin")
+                .password(encoder.encode("admin123"))
+                .roles("ADMIN")
+                .build());
+
+        // Student User
+        userDetailsManager.createUser(User.builder()
+                .username("student")
+                .password(encoder.encode("student123"))
+                .roles("STUDENT")
+                .build());
+
+        return userDetailsManager;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
                 .antMatchers("/studentInfo/welcome").permitAll() // Allow access to /welcome without authentication
                 .anyRequest().authenticated()
                 .and()
-                .formLogin()
-                .and()
                 .httpBasic()
                 .and()
                 .csrf().disable();
-    }
 
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth
-                .inMemoryAuthentication()
-                .withUser("admin").password("{noop}password").roles("ADMIN");
+        return http.build();
     }
 }
